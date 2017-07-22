1b6ecc17c299a2c9e44a47f72fb1a72fe1f638b8
Nesh108 <chenhao@163.com>
2017-07-22 19:14:19 +0800

    added fetching from bacfrom backend

--- Assets/Scripts/Droppable.cs
+++ Assets/Scripts/Droppable.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class Droppable
 {	
+	public string Parent;
 	public string Marker;
 	public DroppableType Dt;
 	public string UserName;
@@ -10,8 +11,9 @@ public class Droppable
 	public string Content;
 	public Vector3 Position;
 
-	public Droppable (string m, DroppableType drop, string username, DateTime date, string c, Vector3 pos)
+	public Droppable (string p, string m, DroppableType drop, string username, DateTime date, string c, Vector3 pos)
 	{
+		Parent = p;
 		Marker = m;
 		Dt = drop;
 		UserName = username;
--- Assets/Scripts/InteractionHandler.cs
+++ Assets/Scripts/InteractionHandler.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using System;
 using System.Text.RegularExpressions;
 
-public class InteractionHandler : MonoBehaviour {
+public class InteractionHandler : MonoBehaviour
+{
 
 	public InteractableType interactType;
 	public List<Droppable> Drops;
@@ -16,30 +17,36 @@ public class InteractionHandler : MonoBehaviour {
 	private TextMesh droppedBy;
 	private SpriteRenderer spriteImage;
 
+	public GameObject TextPrefab;
+	public GameObject ImagePrefab;
+	public GameObject AudioPrefab;
+	public GameObject EmojiPrefab;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		textbox = GameObject.FindGameObjectWithTag ("Label");
 		droppedBy = GetComponentInChildren<TextMesh> ();
 		spriteImage = GetComponentInChildren<SpriteRenderer> ();
 		textboxImage = textbox.GetComponent<UnityEngine.UI.Image> ();
-		Drops = DroppableFetcher.FetchSpecificDroppables (gameObject.name);
+		StartCoroutine (DroppableFetcher.FetchSpecificDroppables (this, gameObject.name, interactType.Equals (InteractableType.MAIN)));
 		label = GameObject.FindObjectOfType<UnityEngine.UI.Text> ();
 
-		if (spriteImage != null && interactType.Equals(InteractableType.DROPPABLE) && Drops[0].Dt.Equals(DroppableType.IMAGE)) {
-			byte[] img = System.Convert.FromBase64String(Drops[0].Content);
+		if (spriteImage != null && interactType.Equals (InteractableType.DROPPABLE) && Drops [0].Dt.Equals (DroppableType.IMAGE)) {
+			byte[] img = System.Convert.FromBase64String (Drops [0].Content);
 			Texture2D tex = new Texture2D (400, 400);
 			tex.LoadImage (img);
-			Sprite s = Sprite.Create (tex, new Rect (0, 0, tex.width, tex.height), new Vector2());
+			Sprite s = Sprite.Create (tex, new Rect (0, 0, tex.width, tex.height), new Vector2 ());
 			spriteImage.sprite = s;
 		}
 	}
 
 	// Update is called once per frame
-	void Update () {
-		if((Input.touchCount>0 && Input.GetTouch(0).phase==TouchPhase.Began) || Input.GetMouseButtonDown(0))
-		{
+	void Update ()
+	{
+		if ((Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) || Input.GetMouseButtonDown (0)) {
 			#if UNITY_EDITOR
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			#elif UNITY_ANDROID
 				Ray ray = Camera.main.ScreenPointToRay (Input.GetTouch(0).position);
 			#endif
@@ -51,7 +58,7 @@ public class InteractionHandler : MonoBehaviour {
 				} else {
 					activated = false;
 					if (interactType.Equals (InteractableType.DROPPABLE)) {
-						if (Drops [0].Dt.Equals (DroppableType.TEXT) && !hit.collider.name.Contains("TEXT#")) {
+						if (Drops [0].Dt.Equals (DroppableType.TEXT) && !hit.collider.name.Contains ("TEXT#")) {
 							Debug.LogWarning ("Disabling shit");
 							textboxImage.enabled = false;
 							label.text = "";
@@ -64,26 +71,29 @@ public class InteractionHandler : MonoBehaviour {
 		}
 	}
 
-	IEnumerator HandleClick(string name) {
-		if(interactType.Equals(InteractableType.MAIN)){
-			DroppableGO.SetActive(!DroppableGO.activeSelf);
-		} else if(interactType.Equals(InteractableType.DROPPABLE)) {
+	IEnumerator HandleClick (string name)
+	{
+		if (interactType.Equals (InteractableType.MAIN)) {
+			DroppableGO.SetActive (!DroppableGO.activeSelf);
+		} else if (interactType.Equals (InteractableType.DROPPABLE)) {
 			activated = !activated;
 			if (Drops.Count > 0 && Drops [0].Dt.Equals (DroppableType.TEXT)) {
 				textboxImage.enabled = activated;
 				label.text = textboxImage.enabled ? Drops [0].UserName + ": " + SpliceText (Drops [0].Content, 20) : "";
 			} else if (Drops.Count > 0 && Drops [0].Dt.Equals (DroppableType.IMAGE)) {
-				droppedBy.text = activated ? Drops [0].UserName: "";
+				droppedBy.text = activated ? Drops [0].UserName : "";
 			}
 		}
 		yield break;
 	}
 
-	public static string SpliceText(string text, int lineLength) {
-		return Regex.Replace(text, "(.{" + lineLength + "})", "$1" + Environment.NewLine);
+	public static string SpliceText (string text, int lineLength)
+	{
+		return Regex.Replace (text, "(.{" + lineLength + "})", "$1" + Environment.NewLine);
 	}
 
-	void OnDisable() {
+	void OnDisable ()
+	{
 		if (textbox != null) {
 			textboxImage.enabled = false;
 		}
@@ -92,9 +102,38 @@ public class InteractionHandler : MonoBehaviour {
 			DroppableGO.SetActive (false);
 		}
 	}
+
+	public void SpawnDroppables ()
+	{
+		if (interactType.Equals (InteractableType.MAIN)) {
+			GameObject go = null;
+			foreach (Droppable d in Drops) {
+				switch (d.Dt) {
+				case DroppableType.TEXT:
+					go = Instantiate (TextPrefab, d.Position, Quaternion.identity, DroppableGO.transform);
+					break;
+				case DroppableType.IMAGE:
+					go = Instantiate (ImagePrefab, d.Position, Quaternion.identity, DroppableGO.transform);
+					break;
+				case DroppableType.AUDIO:
+					go = Instantiate (AudioPrefab, d.Position, Quaternion.identity, DroppableGO.transform);
+					break;
+				case DroppableType.EMOJI:
+					go = Instantiate (EmojiPrefab, d.Position, Quaternion.identity, DroppableGO.transform);
+					break;
+				}
+
+				if (go != null) {
+					go.name = d.Marker;
+				}
+
+			}
+		}
+	}
 }
 
-public enum InteractableType {
+public enum InteractableType
+{
 	MAIN,
 	DROPPABLE
 }
